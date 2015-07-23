1d494ce669a334e73980facbd8d5090a29bcee0b
Piotr Kordy <pkordy@googlemail.com>
2015-07-23 09:12:13 +0200

    Changed default server to tryango.com

--- src/defaults/preferences/tryango_prefs.js
+++ src/defaults/preferences/tryango_prefs.js
@@ -5,7 +5,7 @@
 pref("extensions.tryango.firstStartup", true);
 pref("extensions.tryango.encryptMessages", true);
 pref("extensions.tryango.signMessages", true);
-pref("extensions.tryango.server", "markryan.eu");
+pref("extensions.tryango.server", "tryango.com");
 pref("extensions.tryango.port", 22);
 pref("extensions.tryango.advancedOptions", false);
 pref("extensions.tryango.logfileName", "tryango-proofs.log");
